aebc372b62746ddc24399827193ebab4974bb4ea
abdulraheemnohri <31163557+abdulraheemnohri@users.noreply.github.com>
2026-06-29 21:55:50 +0500

    Add service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'lipsync-ai-v1';
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(['/', '/index.html', '/css/style.css', '/js/app.js']))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
